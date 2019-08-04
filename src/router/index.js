a256c9e26d185740af5f4200d318a4fabe29f51a
ying <future_fe@163.com>
2019-08-04 16:13:56 +0800

    登录模块路由及组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 // 定义router对象  导出给main.js使用
 import VueRouter from 'vue-router'
 import Vue from 'vue'
+import login from '@/views/login'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   // 路由规则配置
-  routes: []
+  routes: [{
+    path: '/login',
+    // name的作用给当前路由规则取名  $router.push('/login')  或者 $router.push({name:'login'})
+    name: 'login',
+    component: login
+  }]
 })
 
 export default router

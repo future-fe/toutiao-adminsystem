a00272dba7e152e36022b3c08eb795a5014049a4
ying <future_fe@163.com>
2019-08-04 17:27:58 +0800

    首页基本完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 // 定义router对象  导出给main.js使用
 import VueRouter from 'vue-router'
 import Vue from 'vue'
-import login from '@/views/login'
-
+import Login from '@/views/login'
+import Home from '@/views/home'
+import Welcome from '@/views/welcome'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -11,7 +12,15 @@ const router = new VueRouter({
     path: '/login',
     // name的作用给当前路由规则取名  $router.push('/login')  或者 $router.push({name:'login'})
     name: 'login',
-    component: login
+    component: Login
+  }, {
+    path: '/',
+    component: Home,
+    children: [{
+      path: '/',
+      name: 'welcome',
+      component: Welcome
+    }]
   }]
 })
 

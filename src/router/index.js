2139952e4ea0d7e436687583e6ef5520a1117e41
ying <future_fe@163.com>
2019-08-04 17:50:11 +0800

    导航菜单路由切换和404页面

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import Login from '@/views/login'
 import Home from '@/views/home'
 import Welcome from '@/views/welcome'
+import NotFound from '@/views/404'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -21,7 +22,13 @@ const router = new VueRouter({
       name: 'welcome',
       component: Welcome
     }]
-  }]
+  }, // 处理404
+  {
+    path: '*',
+    name: '404',
+    component: NotFound
+  }
+  ]
 })
 
 export default router
